b6b30ed532e4f26eaf31c0843536cafd89fabc44
Elgilany Hassan <gellanyhassan0@gmail.com>
2022-07-14 20:58:16 +0000

    Update gellany_ossec.sh

--- gellany_ossec.sh
+++ gellany_ossec.sh
@@ -4,3 +4,6 @@ apt-get install ossec-hids-serve
 service apache2 status
 service apache2 start
 curl -s "http://0.0.0.0/ossec/"
+wget https://raw.githubusercontent.com/gellanyhassan0/gellany_ossec/main/ossec.conf
+cp -r ossec.conf /var/ossec/etc/
+ 
